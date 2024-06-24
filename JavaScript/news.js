b35ac726ed6a9ed9f28767abccfbbb647a5825ed
NoNameGameDevelopment <nonamegamedevelopment@gmail.com>
2024-06-24 19:46:46 +0200

    Update news.js

--- JavaScript/news.js
+++ JavaScript/news.js
@@ -4,14 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // Dummy-Daten für die News (kann durch echte Daten ersetzt werden)
     const newsItems = [
         {
-            title: "1.35 - 23/06/2024 - Neue Funktionen für die Website der KI-Fortschritte und Anwendungen",
-            content: "In den letzten Entwicklungsphasen wurden mehrere wichtige Änderungen am Projekt vorgenommen. Zunächst wurden verschiedene Fehler behoben, die während der Tests identifiziert wurden. Diese Fehlerbehebungen waren entscheidend, um die Stabilität und Zuverlässigkeit der Anwendung sicherzustellen. Darüber hinaus wurde eine neue Funktion implementiert, die es Benutzern ermöglicht, Bilder zu vergrößern, indem sie mit der Maus darüber schweben. Diese Funktion verbessert die Benutzerfreundlichkeit erheblich und bietet eine interaktivere Erfahrung beim Betrachten von Bildmaterial. Ein weiterer signifikanter Schritt war die Überarbeitung der Navigationmenüs. Diese wurden in ein Dropdown-Menü umgewandelt, was die Navigation durch die Anwendung vereinfacht und Platz auf der Benutzeroberfläche spart. Diese Änderung trägt dazu bei, dass die Benutzer intuitiv durch die verschiedenen Abschnitte der Anwendung navigieren können. Diese jüngsten Verbesserungen stellen sicher, dass das Projekt nicht nur funktionaler, sondern auch benutzerfreundlicher wird, indem sie häufige Probleme lösen und die Navigation erleichtern.",
-            date: "23. Juni 2024"
-        },
-        {
-            title: "1.0 - Webseite veröffentlicht",
-            content: "Neue Webseite unter https://ki.bbd-8.org/ verfügbar",
-            data: "19/06/2024",
+            title: "Veröffentlichung von Deep Seek v2: Kostenlos und Open Source!",
+            content: "Deep Seek v2 ist jetzt offiziell verfügbar und erleichtert die Suche nach Code und mathematischen Inhalten erheblich. Diese fortschrittliche Suchtechnologie ist kostenlos und Open Source, sodass jeder den Quellcode einsehen und zur Weiterentwicklung beitragen kann.",
+            date: "Veröffentlicht am 24.06.2024 um 20:00 Uhr"
         },
 
     ];
